217de112ef04237073c651417b6a2483be3b1f9e
Samuel <she@bdiworldwide.com>
2024-04-09 17:19:36 -0400

    add 2211 code

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { MultiCartService } from '@spartacus/cart/base/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  //constructor(private service: MultiCartService) {}
+  title = '2211store';
+}
